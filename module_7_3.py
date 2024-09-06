9ecf188fcbe79e6cfb13df196263b15d17a51ab1
MakashovAV <mak2440@yandex.ru>
2024-09-06 22:12:18 +0700

    # Цель: применить на практике оператор with, вспомнить написание кода в парадигме ООП.

---
+++ module_7_3.py
@@ -0,0 +1,34 @@
+# Цель: применить на практике оператор with, вспомнить написание кода в парадигме ООП.
+# Задача "Найдёт везде":
+
+class WordsFinder:
+    def __init__(self, *file_names):
+        self.file_names = file_names
+
+    def get_all_words(self):
+        all_words = {}
+        punc = [',', '.', '=', '!', '?', ';', ':', ' - ']
+        for file_name in self.file_names:
+            with open(file_name, 'r', encoding='utf-8') as file:
+                words = file.read().lower()
+                for x in punc:
+                    data = words.replace(x, '')
+            all_words[file_name] = data.split()
+        return all_words
+
+    def find(self, word):
+        find_index = {}
+        for file_name, words in self.get_all_words().items():
+            find_index[file_name] = words.index(word.lower()) + 1
+        return find_index
+
+    def count(self, word):
+        count_word = {}
+        for file_name, words in self.get_all_words().items():
+            count_word[file_name] = words.count(word.lower())
+        return count_word
+
+finder2 = WordsFinder('test_file.txt')
+print(finder2.get_all_words())  # Все слова
+print(finder2.find('TEXT')) # 3 слово по счёту
+print(finder2.count('teXT')) # 4 слова teXT в тексте всего
